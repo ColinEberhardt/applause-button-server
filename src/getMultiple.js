cc053d444b0175ab7d873121646971b0e1d3c134
Colin Eberhardt <colin.eberhardt@gmail.com>
2018-05-21 14:46:30 +0100

    fix: removes duplicate URLs

--- src/getMultiple.js
+++ src/getMultiple.js
@@ -1,7 +1,7 @@
 const is = require("is_js");
 const lambda = require("./util/lambda");
 const { getItems } = require("./util/persistence");
-const { isurl, assert } = require("./util/util");
+const { isurl, assert, unique } = require("./util/util");
 
 module.exports.fn = lambda(async (event, success) => {
   const urls = JSON.parse(event.body);
@@ -10,6 +10,6 @@ module.exports.fn = lambda(async (event, success) => {
   assert(urls.every(isurl), "getMultiple requires an array of URLs");
 
   // limit the query to 100 URLs
-  const items = await getItems(urls.slice(0, 100));
+  const items = await getItems(unique(urls.slice(0, 100)));
   success(items);
 });
--- src/getMultiple.test.js
+++ src/getMultiple.test.js
@@ -22,6 +22,15 @@ test("returns the correct response", done => {
   });
 });
 
+test("removes duplicate URLs", done => {
+  const urls = JSON.stringify(["google.com", "google.com", "microsoft.com"]);
+  getMultiple({ body: urls }, undefined, (error, response) => {
+    const body = JSON.parse(response.body);
+    expect(body.length).toBe(2);
+    done();
+  });
+});
+
 test("validates that the request is an array", done => {
   getMultiple({ body: "fish" }, undefined, (error, response) => {
     expect(error).toBe("an error occurred - bad luck!");
@@ -32,7 +41,7 @@ test("validates that the request is an array", done => {
 test("limits the request to 100 URLs", done => {
   const urls = [];
   for (var i = 0; i < 200; i++) {
-    urls.push("google.com");
+    urls.push("google.com/" + i);
   }
   getMultiple({ body: JSON.stringify(urls) }, undefined, (error, response) => {
     const body = JSON.parse(response.body);
