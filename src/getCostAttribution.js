e5a8d2d4a90e0358fe29e094de9bdd65c19270a5
Colin Eberhardt <colin.eberhardt@gmail.com>
2019-02-24 08:38:33 +0000

    feat: allow attribution of costs

---
+++ src/getCostAttribution.js
@@ -0,0 +1,18 @@
+const lambda = require("./util/lambda");
+const { getCostAttribution } = require("./util/attributeCosts");
+
+module.exports.fn = lambda(async (_, success) => {
+  const item = await getCostAttribution();
+  let consumers = item.Items;
+  // top 10 sorted by cost
+  consumers.sort((i, j) => j.cost - i.cost);
+  consumers = consumers.slice(0, 10);
+
+  console.log(consumers);
+
+  if (item.Item) {
+    success(item.Item);
+  } else {
+    success(0);
+  }
+});
